aa1a3e64dd3896b97f9bc8538927f39fadadbb23
Alex Morozov <scanhex@gmail.com>
2024-10-20 21:51:57 +0000

    Try changing UPat.any

--- tinygrad/ops.py
+++ tinygrad/ops.py
@@ -497,7 +497,7 @@ class UPat(MathTrait):
       self.early_reject = set((pp.op[0], pp.arg) for pp in upat_match if pp.op is not None and len(pp.op) == 1)
 
   @staticmethod
-  def any(*src): return UPatAny(src=src)
+  def any(*src): return UPat(src=src)
 
   @staticmethod
   @functools.lru_cache(None)
@@ -545,10 +545,11 @@ class UPat(MathTrait):
     if self.src is None: return [store]
     res: List[Dict[str, UOp]] = []
     for vp in self.src:
-      stores, new_stores = [store.copy()], []
+      stores = [store.copy()]
       for uu, vv in zip(uop.src, vp):
+        new_stores = []
         for s in stores: new_stores.extend(vv.match(uu, s))
-        stores, new_stores = new_stores, []
+        stores = new_stores
       res.extend(stores)
     return res
 
@@ -588,7 +589,7 @@ class PatternMatcher:
   def rewrite(self, uop:UOp, ctx=None) -> Optional[UOp]:
     ler = set([v for u in uop.src for v in ((u.op, u.arg), (u.op, None))])
     for p,fxn,early_reject in self.pdict.get((uop.op, uop.arg), []) + ([] if uop.arg is None else self.pdict.get((uop.op, None), [])):
-      if not early_reject.issubset(ler): continue
+  #    if not early_reject.issubset(ler): continue
       if (matches := p.match(uop, {})) and (ret:=(fxn(ctx, **matches[0]) if ctx is not None else fxn(**matches[0]))) is not None: return ret
     return None
 
