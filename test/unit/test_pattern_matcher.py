6f51a454641eb3a89ee60cb8ed651aa472338fa2
Alexander Morozov <44344009+scanhex@users.noreply.github.com>
2024-10-24 19:30:42 -0400

    Implement simple DFA and DFA union

--- test/unit/test_pattern_matcher.py
+++ test/unit/test_pattern_matcher.py
@@ -1,4 +1,7 @@
-from typing import Any, Set, List
+# vim: set sw=2 ts=2 colorcolumn=0:
+
+from os import walk
+from typing import Any, Pattern, Set, Dict, List, Tuple, Callable
 import unittest, itertools
 from tinygrad.dtype import dtypes
 from tinygrad.ops import UOps, UOp, BinaryOps, TernaryOps, ReduceOps, UnaryOps # noqa: F401
@@ -6,42 +9,99 @@ from tinygrad.ops import PatternMatcher, UPat
 
 import dfa
 
-class State:
-    u: UPat
-    stage_in_uop: str # op, arg, dtype
-
+OTHER_TRANSITION = "OTHER"
 def create_dfa(upat: UPat) -> dfa.DFA:
-    root = 0
-    term = 1
-    class OtherTransition:
-        pass
-    alphabet: Set[Any] = {OtherTransition()}
-    def gather_alphabet(u: UPat):
-        if u.op: alphabet.add(u.op)
-        if u.arg: alphabet.add(u.arg)
-        if u.dtype: alphabet.add(u.dtype)
-        if u.src:
-            for comb in u.src:
-                for v in ((next(comb),) if isinstance(comb, itertools.repeat) else comb):
-                    gather_alphabet(v)
-    gather_alphabet(upat)
-    print(alphabet)
-    return dfa.DFA(
-            start=root,
-            inputs = alphabet,
-            label=lambda x: 1 if x == term else None,
-            transition=lambda s, c: go(s, c),
-            outputs={None, 1}
-            )
+  accept = 0
+  reject = 1
+  alphabet: Set[Any] = {OTHER_TRANSITION}
+  def gather_alphabet(u: UPat):
+    if u.op: alphabet.update(u.op)
+    if u.arg: alphabet.add(u.arg)
+    if u.dtype: alphabet.update(u.dtype)
+    if u.src:
+      for comb in u.src:
+        for v in ((next(comb),) if isinstance(comb, itertools.repeat) else comb):
+          gather_alphabet(v)
+  gather_alphabet(upat)
+  e: Dict = dict()
+  for x in alphabet: e[(reject, x)] = e[(accept, x)] = reject
+  cnt = 2
+  def alloc() -> int: nonlocal cnt; c = cnt; cnt += 1; return c
+  def gen(u: UPat, cb: Callable[[int], None]) -> Tuple[int, Callable[[int], None]]:
+    start, after_op, after_dtype = alloc(), alloc(), alloc()
+    cb(start)
+    accepted = set(u.op) if u.op else alphabet # can switch alphabet-> all_ops, same below
+    for a in alphabet: e[(start, a)] = after_op if a in accepted else reject
+    accepted = set(u.dtype) if u.dtype else alphabet 
+    for a in alphabet: e[(after_op, a)] = after_dtype if a in accepted else reject
+    def connect(x: int):
+      accepted = {u.arg} if u.arg else alphabet
+      for a in alphabet: e[(after_dtype, a)] = x if a in accepted else reject
+    assert u.src # todo handle None
+    assert len(u.src) == 1 # todo unfold multi-src patterns
+    assert not isinstance(u.src[0], itertools.repeat) # todo handle repeats
+    curcb = connect
+    for i, v in enumerate(u.src[0]):
+      v_start, curcb = gen(v, curcb)
+      if i == 0: 
+        accepted = {u.arg} if u.arg else alphabet
+        for a in alphabet: e[(after_dtype, a)] = v_start if a in accepted else reject
+    return start, curcb
 
-def get_dfa_word(_: UOp) -> List:
-    return []
+  root, callback = gen(upat, lambda _: None)
+  callback(accept)
+
+  return dfa.DFA(
+      start=root,
+      inputs=alphabet,
+      label=lambda x: 1 if x == accept else 0,
+      transition=lambda s, c: e[(s, c)],
+      outputs={0, 1}
+  )
+
+def dfa_union(dfas: List[dfa.DFA]) -> dfa.DFA:
+  alphabet = set()
+  for x in dfas:
+    assert x.inputs
+    alphabet.update(x.inputs)
+  e = dict()
+  state_id = dict()
+  cnt = 0
+  labels = dict()
+  def dfs(state: Tuple[int, ...]):
+    nonlocal cnt
+    if (id := state_id.setdefault(state, cnt)) != cnt: return id
+    labels[id] = tuple(i for i, (x, d) in enumerate(zip(state, dfas)) if d._label(x) == 1)
+    cnt += 1
+    for c in alphabet:
+      next_state = tuple(d._transition(s, c if c in d.inputs else OTHER_TRANSITION) for s, d in zip(state, dfas))
+      e[(state_id[state], c)] = dfs(next_state)
+    return id
+
+  start = dfs(tuple(int(d.start) for d in dfas))
+  return dfa.DFA(
+      start=start,
+      inputs=alphabet,
+      label=lambda x: labels[x],
+      transition=lambda s, c: e[(s, c)],
+      outputs=labels.values()
+  )
+
+
+def create_multi_dfa(upats: List[UPat]) -> dfa.DFA:
+  return dfa_union([create_dfa(u) for u in upats])
+
+def get_dfa_word(dfa: dfa.DFA, u: UOp) -> List:
+  assert dfa.inputs
+  return [x if x in dfa.inputs else OTHER_TRANSITION for x in (u.op, u.dtype, u.arg)] +\
+      list(itertools.chain.from_iterable(map(lambda x: get_dfa_word(dfa, x), u.src)))
 
 class TestPatternMatcher(unittest.TestCase):
   def test_dfa(self):
-      upat = UPat(UOps.CONST, name="x", dtype=dtypes.float)
-      dfa = create_dfa(upat)
-      assert dfa.label(get_dfa_word(UOp(UOps.CONST, dtypes.float, arg=1.0))) == 1
+    upats = [UPat(UOps.CONST, name="x", dtype=dtypes.float, src=()), UPat(UOps.CONST, name="x", dtype=dtypes.float, src=())];
+    dfa = create_multi_dfa(upats)
+    assert dfa.label(get_dfa_word(dfa, UOp(UOps.CONST, dtypes.float, arg=1.0))) == (0,1)
+    assert dfa.label(get_dfa_word(dfa, UOp(UOps.CONST, dtypes.int, arg=1.0))) == ()
 
   def test_simple_match(self):
     matcher = PatternMatcher([(UPat(UOps.CONST, name="x", dtype=dtypes.float), lambda x: x)])
